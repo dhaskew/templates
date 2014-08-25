39abaf880ea0a5db2d7d0c04ab2f7aa05dedae93
David Askew <davidaskew@gmail.com>
2014-08-25 15:38:31 -0400

    Updated rails template

--- rails.rb
+++ rails.rb
@@ -16,4 +16,37 @@
 
 #############  START ACTUAL CODE ##############
 
+gem 'haml-rails'
+
+gem_group :development do
+  gem 'annotate'           #adds table info to models via 'annotate' command
+  gem 'better_errors'      #better landing page for errors
+  gem 'binding_of_caller'  #command line on better landing page
+  gem 'pry-rails'          #integrate pry, specifically for "rails c"
+  gem 'quiet_assets'
+  gem 'spring'
+  #gem 'bullet'
+  gem 'erb2haml'           #rake haml:replace_erbs
+end
+
+case ask("Choose database engine:(postgres, sqlite):", :limited_to => %w[postgres sqlite])
+when "postgres"
+  gem 'pg'
+when "sqlite"
+  gem 'sqlite'
+end
+
+#install extra gems
+run "bundle install"
+
+#convert erb default files to haml
+rake "haml:replace_erbs"
+
+
+#git setup
+
+git :init
+git add: "."
+git commit: %Q{ -m 'Initial Commit' }
+
 
