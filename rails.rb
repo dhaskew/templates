c8d70c5e41e3b7bd26ff20121e7da10f1ef0e654
David Askew <davidaskew@gmail.com>
2014-08-25 15:14:28 -0400

    Added Setup Notes

--- rails.rb
+++ rails.rb
@@ -1,2 +1,19 @@
-#rvm gemset --create use <project_name>
-#gem install rails
+############# SETUP NOTES ####################
+
+# setup rvm
+# rvm gemset --create use <project_name>
+
+# install rails
+# gem install rails
+
+# execute this script
+# rails new <project_name> -m <path_to_template>
+# rails new <project_name> -m ./local_file.rb
+# rails new <project_name> -m https://raw.githubusercontent.com/dhaskew/templates/master/rails.rb
+
+# useful bash alias idea
+# alias rtemp='curl https://raw.githubusercontent.com/dhaskew/templates/master/rails.rb'
+
+#############  START ACTUAL CODE ##############
+
+
